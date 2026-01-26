0c65fee91d6fc2da3def78791622da23c99546c3
Subhranshu <subhranshumitra2004@gmail.com>
2026-01-27 02:20:55 +0530

    Add solution for Search a 2D Matrix II

--- LeetCode/Search-a-2D-Matrix-II/Solution.cpp
+++ LeetCode/Search-a-2D-Matrix-II/Solution.cpp
@@ -1,25 +1,16 @@
 1class Solution {
 2public:
-3    bool binarySearch(vector<int>& v,int target){
-4        int low= 0;
-5        int high= v.size()-1;
-6        int mid;
-7
-8        while(low<=high){
-9            mid= low +(high-low)/2 ;
-10
-11            if(v[mid]==target) return true;
-12            else if(v[mid]>target) high=mid-1;
-13            else low=mid+1;
-14        }
-15        return false;
-16    }
-17    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-18        for(int i=0;i<matrix.size();i++){
-19            //vector<int> v=matrix[i];
-20            bool b= binarySearch (matrix[i],target);
-21            if(b) return true;
-22        }
-23        return false;
-24    }
-25};
+3    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+4        int m=matrix.size();
+5        int n=matrix[0].size();
+6        int row=0;
+7        int col=n-1;
+8        
+9        while(row<m && col>=0){
+10            if(matrix[row][col]==target) return true;
+11            else if(matrix[row][col]>target) col--;
+12            else row++;
+13        }
+14        return false;
+15    }
+16};
