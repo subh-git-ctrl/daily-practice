0012e87b44bd4c8642f6a520fc9bd4e22ddba84f
Subhranshu <subhranshumitra2004@gmail.com>
2026-02-27 03:40:19 +0530

    Add solution for Daily Temperatures

--- LeetCode/Daily-Temperatures/Solution.cpp
+++ LeetCode/Daily-Temperatures/Solution.cpp
@@ -6,14 +6,9 @@
 6        vector<int> arr(len,0);
 7        for(int i=len-1;i>=0;i--){
 8            
-9            while(!st.empty() && st.top().first<=temperatures[i]) st.pop();
-10
-11            if(st.empty()) arr[i]=0;
-12            else arr[i]= st.top().second - i;
-13
-14            st.push({temperatures[i],i});
-15            
-16        }
-17        return arr;
-18    }
-19};
+9            if(st.empty()){ arr[i]=0; st.push({temperatures[i],i}); } else { while(!st.empty() && st.top().first<=temperatures[i]) st.pop(); if(st.empty()){ arr[i]=0; st.push({temperatures[i],i}); } else { arr[i]= st.top().second - i; st.push({temperatures[i],i}) ; } }
+10            
+11        }
+12        return arr;
+13    }
+14};
