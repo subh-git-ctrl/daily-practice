a5e704acae11c63bba398c600c7804587a828028
Subhranshu <subhranshumitra2004@gmail.com>
2026-06-02 20:26:23 +0530

    Add solution for Permutations

--- LeetCode/Permutations/Solution.cpp
+++ LeetCode/Permutations/Solution.cpp
@@ -1,29 +1,22 @@
 1class Solution {
 2public:
-3    void Perms(int i,vector<int>& nums,int n,vector<bool> &visited,vector<int> &ds,vector<vector<int>> &result){
-4        if(ds.size()==n){
-5            result.push_back(ds);
+3    void Perms(vector<int>& nums,int index,int n,vector<vector<int>> &result ){
+4        if(index==n){
+5            result.push_back(nums);
 6            return ;
 7        }
 8
-9        for(int ind=0;ind<n;ind++){
-10            if(!visited[ind]){
-11                visited[ind]=true;
-12                ds.push_back(nums[ind]);
-13                Perms(i,nums,n,visited,ds,result);
-14                ds.pop_back();
-15                visited[ind]=false;
-16                //Perms(i,nums,n,visited,ds,result);
-17            }
-18        }
-19    }
-20    vector<vector<int>> permute(vector<int>& nums) {
-21        int n=nums.size();
-22        vector<bool> visited(n,false);
-23        vector<int> ds;
-24        vector<vector<int>> result;
-25
-26        Perms(0,nums,n,visited,ds,result);
-27        return result;
-28    }
-29};
+9        for(int i=index;i<n;i++){
+10            swap(nums[index],nums[i]);
+11            Perms(nums,index+1,n,result);
+12            swap(nums[index],nums[i]);
+13        }
+14    }
+15    vector<vector<int>> permute(vector<int>& nums) {
+16        vector<int> ans;
+17        vector<vector<int>> result;
+18        int n=nums.size();
+19        Perms(nums,0,n,result);
+20        return result;
+21    }
+22};
