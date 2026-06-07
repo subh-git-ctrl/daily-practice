923cdb1c8c7bf699db6734c6eca87b7c77f75114
Subhranshu <subhranshumitra2004@gmail.com>
2026-06-07 21:17:40 +0530

    Add solution for Left and Right Sum Differences

--- LeetCode/Left-and-Right-Sum-Differences/Solution.cpp
+++ LeetCode/Left-and-Right-Sum-Differences/Solution.cpp
@@ -1,18 +1,20 @@
-1class Solution {
-2public:
-3    vector<int> leftRightDifference(vector<int>& nums) {
-4        int n=nums.size();
-5        vector<int> leftSum(n);
-6        vector<int> rightSum(n);
-7        vector<int> ans(n);
-8
-9        for(int i=1;i<n;i++){
-10            leftSum[i]=leftSum[i-1]+nums[i-1];
-11            rightSum[n-i-1]=rightSum[n-i]+nums[n-i];
-12        }
-13        for(int i=0;i<n;i++){
-14            ans[i]=abs(leftSum[i]-rightSum[i]);
-15        }
-16        return ans;
-17    }
-18};
+class Solution {
+public:
+    vector<int> leftRightDifference(vector<int>& nums) {
+        int right_sum=0;
+
+        for(int i=0;i<nums.size();i++){
+            right_sum+=nums[i];
+        }
+
+        int left_sum=0;
+        vector<int> result;
+        for(int i=0;i<nums.size();i++){
+            right_sum -= nums[i];
+            result.push_back(abs(left_sum-right_sum));
+            left_sum += nums[i];
+        }
+
+        return result;
+    }
+};
