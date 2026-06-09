b2be4c3e86a27e9ac95292bf4c7fbbfd76f1459d
Subhranshu <subhranshumitra2004@gmail.com>
2026-06-10 00:55:42 +0530

    Add solution for Minimum Path Sum

--- LeetCode/Minimum-Path-Sum/Solution.cpp
+++ LeetCode/Minimum-Path-Sum/Solution.cpp
@@ -13,7 +13,21 @@
 13    int minPathSum(vector<vector<int>>& grid) {
 14        int m=grid.size();
 15        int n=grid[0].size();
-16        vector<vector<int>> dp(m,vector<int> (n,-1));
-17        return f(0,0,m,n,grid,dp);
-18    }
-19};
+16        vector<vector<int>> dp(m,vector<int> (n,0));
+17        //return f(0,0,m,n,grid,dp);
+18        dp[m-1][n-1]=grid[m-1][n-1];
+19        for(int i=m-1;i>=0;i--){
+20            for(int j=n-1;j>=0;j--){
+21                if(i==m-1 && j==n-1) continue;
+22                else{
+23                    int curr=grid[i][j];
+24                    int right=1e9,down=1e9;
+25                    if(j+1<=n-1) right= curr+dp[i][j+1];
+26                    if(i+1<=m-1) down = curr+dp[i+1][j];
+27                    dp[i][j] = min(right,down);
+28                }
+29            }
+30        }
+31        return dp[0][0];
+32    }
+33};
