b28cde050c828f60c094bc83270c7af501d46a84
Subhranshu <subhranshumitra2004@gmail.com>
2026-06-24 01:05:34 +0530

    Add solution for Lowest Common Ancestor of a Binary Tree

--- LeetCode/Lowest-Common-Ancestor-of-a-Binary-Tree/Solution.cpp
+++ LeetCode/Lowest-Common-Ancestor-of-a-Binary-Tree/Solution.cpp
@@ -9,31 +9,14 @@
 9 */
 10class Solution {
 11public:
-12    bool path_root_to_node(TreeNode* root, TreeNode* node,vector<TreeNode*> &path){
-13        if(root==NULL) return false;
-14        path.push_back(root);
-15        if(root==node) return true;
-16
-17        
-18        if(path_root_to_node(root->left,node,path) || path_root_to_node(root->right,node,path) )
-19            return true;
-20        
-21        path.pop_back();
-22        return false;
-23    }
-24    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-25        if(root==NULL) return NULL;
-26        vector<TreeNode*> path1,path2;
-27        bool p1=path_root_to_node(root,p,path1);
-28        bool p2=path_root_to_node(root,q,path2);
-29
-30        TreeNode* lca=root;
-31        int mini_length= min(path1.size(),path2.size());
-32
-33        for(int i=1;i<mini_length;i++){
-34            if(path1[i]==path2[i]) lca=path1[i];
-35            else break;
-36        }
-37        return lca;
-38    }
-39};
+12    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+13        if(root==NULL || root==p || root==q) return root;
+14
+15        TreeNode* leftN= lowestCommonAncestor(root->left,p,q);
+16        TreeNode* rightN= lowestCommonAncestor(root->right,p,q);
+17
+18        if(leftN!= NULL && rightN!=NULL) return root;
+19        else if(leftN==NULL) return rightN;
+20        else return leftN;
+21    }
+22};
