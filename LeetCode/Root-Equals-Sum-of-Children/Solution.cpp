13f4757abde9f201b950fddb0c8899bcea137ef7
Subhranshu <subhranshumitra2004@gmail.com>
2026-06-26 21:21:57 +0530

    Add solution for Root Equals Sum of Children

---
+++ LeetCode/Root-Equals-Sum-of-Children/Solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public boolean checkTree(TreeNode root) {
+if(root==null) return true;
+if(root.left==null&&root.right==null)
+     return true;
+    int sum=0;
+     if(root.left!=null)
+      sum+=root.left.val;
+        
+      if(root.right!=null) 
+      sum+=root.right.val;
+       
+        return
+(sum==root.val&&checkTree(root.left)&&checkTree(root.right));
+    }
+}
