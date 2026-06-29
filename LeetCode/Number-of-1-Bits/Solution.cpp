714d72eca394ff9b76c5ef1d24c8dc432a020278
Subhranshu <subhranshumitra2004@gmail.com>
2026-06-29 13:25:22 +0530

    Add solution for Number of 1 Bits

--- LeetCode/Number-of-1-Bits/Solution.cpp
+++ LeetCode/Number-of-1-Bits/Solution.cpp
@@ -3,11 +3,13 @@
 3    int hammingWeight(int n) {
 4        int m=n;
 5        int count=0;
-6        while(m!=0){
-7            int rem=m%2;
-8            if(rem==1) count++;
-9            m/=2;
-10        }
-11        return count;
-12    }
-13};
+6
+7        while(m!=0){
+8            int rem= m%2;
+9            if(rem==1) count++;
+10            m/=2;
+11        }
+12
+13        return count;
+14    }
+15};
