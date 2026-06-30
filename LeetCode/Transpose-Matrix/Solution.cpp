189f0fcc6ca94be4ab14036f74874ed74718bc5d
Subhranshu <subhranshumitra2004@gmail.com>
2026-06-30 23:37:16 +0530

    Add solution for Transpose Matrix

--- LeetCode/Transpose-Matrix/Solution.cpp
+++ LeetCode/Transpose-Matrix/Solution.cpp
@@ -4,9 +4,9 @@
 4        int r=matrix.size();
 5        int c=matrix[0].size();
 6        vector<vector<int>> new_matrix(c, vector<int> (r,-1));
-7        for(int i=0;i<r;i++){
-8            for(int j=0;j<c;j++){
-9                new_matrix[j][i] = matrix[i][j];
+7        for(int col=0;col<c;col++){
+8            for(int row=0;row<r;row++){
+9                new_matrix[col][row] = matrix[row][col];
 10            }
 11        }
 12        return new_matrix;
